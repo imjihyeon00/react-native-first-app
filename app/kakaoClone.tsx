0c7488390fc4d48163069003920130f56a579b76
imjihyeon00 <imjihyeon00@gmail.com>
2025-07-14 14:01:06 +0900

    <ScrollView>를 <FlatList>로 변경

--- app/kakaoClone.tsx
+++ app/kakaoClone.tsx
@@ -1,14 +1,13 @@
-import MyProfile from '@/components/kakaoClone/Profile';
-import Header from '@components/kakaoClone/Header';
 import React, { useState } from 'react';
 // import { SafeAreaView } from 'react-native';
 import Division from '@/components/kakaoClone/Division';
-import FriendList from '@/components/kakaoClone/FriendList';
 import FriendSection from '@/components/kakaoClone/FriendSection';
+import Header from '@/components/kakaoClone/Header';
 import Margin from '@/components/kakaoClone/Margin';
+import Profile from '@/components/kakaoClone/Profile';
 import TabBar from '@/components/kakaoClone/TabBar';
 import { friendProfiles, myProfile } from '@lib/data';
-import { View } from 'react-native';
+import { FlatList, View } from 'react-native';
 import {
   SafeAreaProvider,
   SafeAreaView,
@@ -33,36 +32,96 @@ export default function MakeView() {
     setIsOpen(!isOpen);
   };
 
+
+  const renderItem = ({ item, index }:{item:any, index: number}) => (
+    <Profile
+      uri={item.uri}
+      name={item.name}
+      introduction={item.introduction}
+    />
+  );
+
+  const ItemSeparatorComponent = () => <Margin height={13} />
+
+  const ListHeaderComponent = () => (
+    <View style={{ backgroundColor: "#fff" }}>
+      <Header />
+      <Margin height={10} />
+      <Profile
+        uri={myProfile.uri}
+        name={myProfile.name}
+        introduction={myProfile.introduction}
+      />
+      <Margin height={15} />
+      <Division />
+      <Margin height={12} />
+
+      <FriendSection
+        friendProfileLen={friendProfiles.length}
+        onPress={onPressFriendArrow}
+        isOpen={isOpen}
+      />
+
+      <Margin height={5} />
+    </View>
+  );
+
+
+  const ListFooterComponent = () => <Margin height={10} />;
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container} edges={['left', 'right']}>
-        <View style={{ flex: 1, paddingHorizontal: 15}}>
-          <Header />
-          <Margin height={10} />
-          <MyProfile
-            uri={myProfile.uri}
-            name={myProfile.name}
-            introduction={myProfile.introduction}
-          />
-          <Margin height={15} />
-          <Division />
-          <Margin height={12} />
-
-          <FriendSection
-            friendProfileLen={friendProfiles.length}
-            onPress={onPressFriendArrow}
-            isOpen={isOpen}
-          />
-          <FriendList
-            data={friendProfiles}
-            isOpen={isOpen}
-          />
-        </View> 
-        <TabBar 
+        <FlatList
+          data={isOpen ? friendProfiles : []}
+          contentContainerStyle={{ paddingHorizontal: 15 }}
+          keyExtractor={( _, index) => index.toString()}
+          ItemSeparatorComponent={ItemSeparatorComponent}
+          renderItem={renderItem}
+          stickyHeaderIndices={[0]} // 헤더를 고정시키기 위해 사용
+          ListHeaderComponent={ListHeaderComponent}
+          ListFooterComponent={ListFooterComponent}
+          showsVerticalScrollIndicator={false}
+        />
+        <TabBar
           selectedTabIdx={selectedTabIdx} 
           setSelectedTabIdx={setSelectedTabIdx}
         />
       </SafeAreaView>
     </SafeAreaProvider>
   );
+
+  // 기존 코드를 이용하여 FlatList로 변경할 수 있습니다.
+  // return (
+  //   <SafeAreaProvider>
+  //     <SafeAreaView style={styles.container} edges={['left', 'right']}>
+  //       <View style={{ flex: 1, paddingHorizontal: 15}}>
+  //         <Header />
+  //         <Margin height={10} />
+  //         <MyProfile
+  //           uri={myProfile.uri}
+  //           name={myProfile.name}
+  //           introduction={myProfile.introduction}
+  //         />
+  //         <Margin height={15} />
+  //         <Division />
+  //         <Margin height={12} />
+
+  //         <FriendSection
+  //           friendProfileLen={friendProfiles.length}
+  //           onPress={onPressFriendArrow}
+  //           isOpen={isOpen}
+  //         />
+  //         <FriendList
+  //           data={friendProfiles}
+  //           isOpen={isOpen}
+  //         />
+  //       </View> 
+  //       <TabBar 
+  //         selectedTabIdx={selectedTabIdx} 
+  //         setSelectedTabIdx={setSelectedTabIdx}
+  //       />
+  //     </SafeAreaView>
+  //   </SafeAreaProvider>
+  // );
 }
